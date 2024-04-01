07f8e6b9006144bb2e8bd3de338dbdc8fffd0021
ilya <ilyamalahov038@gmail.com>
2024-04-01 20:29:50 +0300

    add

---
+++ catalog/templates/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from catalog.views import home, contacts
+
+urlpatterns = [path('', home),
+               path('contacts/', contacts, name='contacts')]
---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"{name} {email}: {message}")
+    return render(request, 'catalog/contacts.html')
