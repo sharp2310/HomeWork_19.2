4aec4b1b6aa2b03fe9a7def5d1afb1a58fb0ddd4
sharp2310 <ilyamalahov038@gmail.com>
2024-05-21 17:34:06 +0300

    CORR HW 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contact, product, create
 from catalog.apps import MainappConfig
+from catalog.views import home, contact, products_list,  create
 
 app_name = MainappConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contact, name='contact'),
-    path('product/<int:pk>', product, name='product'),
+    path('product/<int:pk>', products_list, name='product'),
     path('create/', create, name='create'),
 ]
